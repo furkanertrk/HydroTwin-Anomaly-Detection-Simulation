c550d3f7311480a468833d91d1b484eb82447923
Furkan ERTURK <furkanerturk789@gmail.com>
2026-03-16 21:43:58 +0300

    yorum satırları silindi

--- model eğitimi/egitilen_model_calistirma.py
+++ model eğitimi/egitilen_model_calistirma.py
@@ -1,17 +1,13 @@
 import joblib
-#Modeli Başka Bir Dosyada/Zamanda Yükleme (Sanki bilgisayarı yeni açmışsın gibi)
 
 print("\n[SİSTEM] Kaydedilen model diskten okunuyor...")
 
 yuklenen_model = joblib.load('hydrotwin_rf_model.pkl')
 
-# 3. Yüklenen Model ile Gerçek Zamanlı Test (Simülasyon)
 
-# Diyelim ki sensörden anlık olarak '1390.5' debi verisi geldi. Bakalım AI ne diyecek?
 
-anlik_sensor_verisi = [[1390.5]] # Çift köşeli parantez önemli (2D array bekler)
+anlik_sensor_verisi = [[1390.5]]
 
-# Sızıntı var mı yok mu tahmin et (0 veya 1 döner)
 
 anlik_tahmin = yuklenen_model.predict(anlik_sensor_verisi)
 
--- model eğitimi/egitim_xgboost.py
+++ model eğitimi/egitim_xgboost.py
@@ -10,25 +10,17 @@ import joblib
 print("[SİSTEM] Temiz Baseline Verisi Yükleniyor...")
 df = pd.read_csv('hydrotwin_clean_data.csv')
 
-# 1. ETİKETLEME (Hedef Değişken)
-# Gelecekte buraya "0: Normal", "1: Küçük Sızıntı", "2: Büyük Sızıntı" ekleyeceğiz.
-# Şimdilik temiz verimizle İkili Sınıflandırma (Binary Classification) yapıyoruz.
 df['Severity_Label'] = (df['Time'] >= 50).astype(int)
 
-# 2. ÖZELLİKLER (Features) ve BÖLME İŞLEMİ
 X = df[['Flow_Rate']]
 y = df['Severity_Label']
 
-# Veriyi %70 Eğitim, %30 Test olarak ayırıyoruz
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
-# 3. XGBOOST MODELİNİN EĞİTİMİ (7. Hafta Hedefi)
 print("\n[AI] XGBoost 'Uzman Cerrah' Modeli Eğitiliyor...")
-# n_estimators: Ağaç sayısı, learning_rate: Öğrenme hızı
 xgb_model = xgb.XGBClassifier(n_estimators=100, learning_rate=0.1, max_depth=5, random_state=42)
 xgb_model.fit(X_train, y_train)
 
-# 4. TEST VE BAŞARI ÖLÇÜMÜ
 print("[AI] Test verileri üzerinde sınıflandırma yapılıyor...")
 y_pred = xgb_model.predict(X_test)
 
@@ -40,7 +32,6 @@ print(f"==========================================")
 print("\nDetaylı Sınıflandırma Raporu (Precision & Recall):")
 print(classification_report(y_test, y_pred, target_names=['Normal Akış (0)', 'Sızıntı (1)']))
 
-# 5. KARMAŞIKLIK MATRİSİ (Confusion Matrix)
 cm = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(6, 5))
 sns.heatmap(cm, annot=True, fmt='d', cmap='Purples', cbar=False, 
@@ -49,6 +40,5 @@ sns.heatmap(cm, annot=True, fmt='d', cmap='Purples', cbar=False,
 plt.title('XGBoost: Sızıntı Sınıflandırma Matrisi', fontweight='bold')
 plt.show()
 
-# 6. MODELİ KAYDET
 joblib.dump(xgb_model, 'hydrotwin_xgboost.pkl')
-print("\n[BAŞARILI] XGBoost Modeli 'hydrotwin_xgboost.pkl' olarak kaydedildi! 7. Hafta Tamamlandı.")
+print("\n[BAŞARILI] XGBoost Modeli 'hydrotwin_xgboost.pkl' olarak kaydedildi!")
